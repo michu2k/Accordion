ef0eece558077506b02621dc98cbb573683a1a12
michu2k <michal.strumpf@gmail.com>
2026-06-23 17:24:02 +0200

    tests: Added more accessibility tests

--- tests/accessibility.test.ts
+++ tests/accessibility.test.ts
@@ -1,4 +1,6 @@
-import { beforeEach, describe, expect, test, vi } from "vitest";
+import { beforeAll, beforeEach, describe, expect, test, vi } from "vitest";
+import { userEvent } from "@testing-library/user-event";
+import { screen } from "@testing-library/dom";
 import { type AccordionConstructor } from "../src/accordion.ts";
 import { createAccordionLayout } from "./test.utils.ts";
 
@@ -9,14 +11,14 @@ beforeEach(async () => {
   Accordion = (await import("../src/accordion.ts")).default;
 });
 
-describe("Accordion accessibility", () => {
+describe("Accordion accessibility - ARIA attributes", () => {
   test("panel and trigger have correct ARIA attributes", () => {
     const { selector, elements } = createAccordionLayout();
     new Accordion(selector);
 
     for (const element of elements) {
-      const trigger = element.querySelector(".ac-trigger")!;
-      const panel = element.querySelector(".ac-panel")!;
+      const trigger = element.querySelector<HTMLElement>(".ac-trigger")!;
+      const panel = element.querySelector<HTMLElement>(".ac-panel")!;
 
       expect(trigger.role).toBe("button");
       expect(trigger.getAttribute("aria-controls")).toBe(panel.id);
@@ -43,8 +45,8 @@ describe("Accordion accessibility", () => {
       }
 
       for (const element of elements) {
-        const trigger = element.querySelector(".ac-trigger")!;
-        const panel = element.querySelector(".ac-panel")!;
+        const trigger = element.querySelector<HTMLElement>(".ac-trigger")!;
+        const panel = element.querySelector<HTMLElement>(".ac-panel")!;
 
         expect(trigger.role).toBeNull();
         expect(trigger.getAttribute("aria-controls")).toBeNull();
@@ -61,7 +63,7 @@ describe("Accordion accessibility", () => {
     const { selector, elements } = createAccordionLayout();
     new Accordion(selector, { openOnInit: [0] });
 
-    const trigger = elements[0]!.querySelector(".ac-trigger")!;
+    const trigger = elements[0]!.querySelector<HTMLElement>(".ac-trigger")!;
 
     expect(trigger.ariaExpanded).toBe("true");
     expect(trigger.ariaDisabled).toBe("false");
@@ -71,9 +73,36 @@ describe("Accordion accessibility", () => {
     const { selector, elements } = createAccordionLayout();
     new Accordion(selector, { openOnInit: [0], collapse: false });
 
-    const trigger = elements[0]!.querySelector(".ac-trigger")!;
+    const trigger = elements[0]!.querySelector<HTMLElement>(".ac-trigger")!;
 
     expect(trigger.ariaExpanded).toBe("true");
     expect(trigger.ariaDisabled).toBe("true");
+  });
+});
+
+describe("Accordion accessibility - keyboard navigation", () => {
+  const user = userEvent.setup();
+
+  beforeAll(() => {
+    const { selector } = createAccordionLayout();
+    new Accordion(selector);
+  });
+
+  test.each([
+    ["{ArrowDown}", "moves focus to the next element", 0, 1],
+    ["{ArrowDown}", "on the last element moves focus to the first element", 2, 0],
+    ["{ArrowUp}", "moves focus to the previous element", 2, 1],
+    ["{ArrowUp}", "on the first element moves focus to the last element", 0, 2],
+    ["{Home}", "moves focus to the first element", 2, 0],
+    ["{End}", "moves focus to the last element", 0, 2]
+  ])("pressing %s key %s", async (key, _, beforeIndex, afterIndex) => {
+    const triggers = screen.getAllByRole("button", { name: /Trigger/i });
+    triggers[beforeIndex]!.focus();
+
+    expect(triggers[beforeIndex]).toHaveFocus();
+
+    await user.keyboard(key);
+
+    expect(triggers[afterIndex]).toHaveFocus();
   });
 });
